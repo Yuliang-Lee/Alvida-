52d433462933d0580a7c316f4cafcf8bc48bc058
Frank.Lee <frank_yll@163.com>
2015-11-06 14:41:20 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -15,6 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
+
       'src/Alvida.js',
       'test/objects.js',
       'test/cross-document.js'
